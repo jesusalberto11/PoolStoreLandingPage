efc098cf3fec78694baa576db306846c8d8a82c0
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-11-25 14:15:02 -0500

    Feat: Implemented translation

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -5,9 +5,12 @@ import TitleAndLogo from "./TitleAndLogo";
 import SimpleButton from "../shared/SimpleButton";
 import { useSidebar } from "../../hooks/useSidebar";
 import { SVG_ICONS } from "../../helpers/svgIcons";
+import { useTranslation } from "react-i18next";
+import LanguageSelector from "../shared/LanguageSelector";
 
 const AppHeader = () => {
   const { isSidebarOpen, openSidebar, closeSidebar } = useSidebar();
+  const [t] = useTranslation("global");
 
   return (
     <header className="header">
@@ -21,7 +24,7 @@ const AppHeader = () => {
       ></div>
       <SimpleButton
         id="toggle-sidebar"
-        title={"Abrir menú de navegación"}
+        title={t("HEADER.OPEN-SIDEBAR-BUTTON-TITLE")}
         icon={SVG_ICONS.THREE_LINES}
         action={openSidebar}
       />
@@ -29,19 +32,20 @@ const AppHeader = () => {
       <nav className="navbar">
         <ul className="nav-items">
           <li className="nav-item centered">
-            <a href="#section-1">Inicio</a>
+            <a href="#section-1">{t("HEADER.NAV-BUTTON-HOME")}</a>
           </li>
           <li className="nav-item centered">
-            <a href="#section-2">Servicios</a>
+            <a href="#section-2">{t("HEADER.NAV-BUTTON-SERVICES")}</a>
           </li>
           <li className="nav-item centered">
-            <a href="#section-3">Testimonios</a>
+            <a href="#section-3">{t("HEADER.NAV-BUTTON-REVIEWS")}</a>
           </li>
           <li className="nav-item centered">
-            <a href="#section-4">Contacto</a>
+            <a href="#section-4">{t("HEADER.NAV-BUTTON-CONTACT")}</a>
           </li>
         </ul>
       </nav>
+      <LanguageSelector />
     </header>
   );
 };
