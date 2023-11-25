65204574195cfa48d14ffa9a6f31afbcbc4f2d6e
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-11-25 15:01:08 -0500

    Feat: Implemented translation

--- src/components/sections/ServiceSection/ServicesSection.jsx
+++ src/components/sections/ServiceSection/ServicesSection.jsx
@@ -8,20 +8,35 @@ import {
   SERVICES_DATA,
   SERVICES_FOOTER_DATA,
 } from "../../../helpers/servicesData";
+import { useTranslation } from "react-i18next";
 
 const ServicesSection = () => {
+  const [t, i18next] = useTranslation("global");
+
   return (
     <CommonSection id={"section-2"}>
-      <SectionTitle firstTitle="Nuestros" secondTitle="Servicios" />
+      <SectionTitle
+        firstTitle={t("SERVICES-SECTION.FIRST-TITLE")}
+        secondTitle={t("SERVICES-SECTION.SECOND-TITLE")}
+      />
       <div className="services-container">
-        {SERVICES_DATA.map(({ id, imgSource, alt, title }) => (
-          <ServiceItem key={id} imgSource={imgSource} alt={alt} title={title} />
-        ))}
+        {SERVICES_DATA[i18next.language.toUpperCase()].map(
+          ({ id, imgSource, alt, title }) => (
+            <ServiceItem
+              key={id}
+              imgSource={imgSource}
+              alt={alt}
+              title={title}
+            />
+          )
+        )}
       </div>
       <div className="services-footer-items centered">
-        {SERVICES_FOOTER_DATA.map(({ id, icon, title }) => (
-          <ServiceFooterItem key={id} icon={icon} title={title} />
-        ))}
+        {SERVICES_FOOTER_DATA[i18next.language.toUpperCase()].map(
+          ({ id, icon, title }) => (
+            <ServiceFooterItem key={id} icon={icon} title={title} />
+          )
+        )}
       </div>
     </CommonSection>
   );
