13d9870715e696e693435ebf6ed487994a1b395d
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-11-25 15:08:50 -0500

    Feat: Implemented translation

--- src/components/sections/ReviewSection/ReviewSection.jsx
+++ src/components/sections/ReviewSection/ReviewSection.jsx
@@ -4,13 +4,19 @@ import CommonSection from "../CommonSection";
 import SectionTitle from "../SectionTitle";
 import ReviewItem from "./ReviewItem";
 import { REVIEWS_DATA } from "../../../helpers/reviewsData";
+import { useTranslation } from "react-i18next";
 
 const ReviewSection = () => {
+  const [t, i18next] = useTranslation("global");
+
   return (
     <CommonSection id={"section-3"}>
-      <SectionTitle firstTitle="Nuestros felices" secondTitle="Clientes" />
+      <SectionTitle
+        firstTitle={t("REVIEWS-SECTION.FIRST-TITLE")}
+        secondTitle={t("REVIEWS-SECTION.SECOND-TITLE")}
+      />
       <div className="reviews-container">
-        {REVIEWS_DATA.map(
+        {REVIEWS_DATA[i18next.language.toUpperCase()].map(
           ({ id, imgSource, alt, username, description, rating }) => (
             <ReviewItem
               key={id}
