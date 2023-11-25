95ee611f3a8aa7690400f4d1e15ccc5bfe21833c
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-11-25 14:37:28 -0500

    Feat: Implemented translation

--- src/components/sections/PoolGridSection/PoolsGridSection.jsx
+++ src/components/sections/PoolGridSection/PoolsGridSection.jsx
@@ -2,35 +2,38 @@ import React from "react";
 import "../../../styles/sections/PoolGridSection/PoolsGridSection.css";
 import CommonSection from "../CommonSection";
 import SectionTitle from "../SectionTitle";
+import { useTranslation } from "react-i18next";
 
 const PoolsGridSection = () => {
+  const [t] = useTranslation("global");
+
   return (
     <CommonSection id={"section-1"}>
       <SectionTitle
-        firstTitle="EXCELENCIA Y CALIDAD"
-        secondTitle="Nuestra filosofia"
+        firstTitle={t("POOL-GRID-SECTION.FIRST-TITLE")}
+        secondTitle={t("POOL-GRID-SECTION.SECOND-TITLE")}
       />
       <div className="pools-grid-rows">
         <div className="pools-grid-columns">
           <img
             src="img/pool-grid/rock-pool.webp"
-            alt="Una hermosa piscina hecha de cuarzo y al lado de una escalera de rocas"
-            title="Una hermosa piscina hecha de cuarzo y al lado de una escalera de rocas"
+            alt={t("POOL-GRID-SECTION.FIRST-IMG-DATA")}
+            title={t("POOL-GRID-SECTION.FIRST-IMG-DATA")}
           />
           <img
             src="img/pool-grid/wow-pool-2.webp"
-            alt="Una hermosa piscina hecha de madera"
-            title="Una hermosa piscina hecha de madera"
+            alt={t("POOL-GRID-SECTION.SECOND-IMG-DATA")}
+            title={t("POOL-GRID-SECTION.SECOND-IMG-DATA")}
           />
           <img
             src="img/pool-grid/cool-pool.jpg"
-            alt="Una hermosa piscina hecha con una pequeña cascada de rocas"
-            title="Una hermosa piscina hecha con una pequeña cascada de rocas"
+            alt={t("POOL-GRID-SECTION.THIRD-IMG-DATA")}
+            title={t("POOL-GRID-SECTION.THIRD-IMG-DATA")}
           />
           <img
             src="img/pool-grid/wow-pool.jpg"
-            alt="Una hermosa piscina en un patio hermoso"
-            title="Una hermosa piscina en un patio hermoso"
+            alt={t("POOL-GRID-SECTION.FOUR-IMG-DATA")}
+            title={t("POOL-GRID-SECTION.FOUR-IMG-DATA")}
           />
         </div>
       </div>
