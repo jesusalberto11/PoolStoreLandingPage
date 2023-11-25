9e0e196927ac5444f4617c272f70734f10289a05
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-11-25 15:00:47 -0500

    Feat: Translated services data items

--- src/helpers/servicesData.js
+++ src/helpers/servicesData.js
@@ -1,51 +1,103 @@
 import { SVG_ICONS } from "./svgIcons";
 
-export const SERVICES_DATA = [
-  {
-    id: 1,
-    title: "Construcción de piscinas",
-    alt: "Servicio de construcción",
-    imgSource: "img/services/service-1.jpg",
-  },
-  {
-    id: 2,
-    title: "Remodelación de piscinas",
-    alt: "Servicio de Remodelación",
-    imgSource: "img/services/service-2.jpg",
-  },
-  {
-    id: 3,
-    title: "Seguridad de piscinas",
-    alt: "Servicio de Seguridad",
-    imgSource: "img/services/service-3.jpg",
-  },
-  {
-    id: 4,
-    title: "Protección de las piscinas",
-    alt: "Servicio de Protección",
-    imgSource: "img/services/service-4.jpg",
-  },
-];
+export const SERVICES_DATA = {
+  ES: [
+    {
+      id: 1,
+      title: "Construcción de piscinas",
+      alt: "Servicio de construcción",
+      imgSource: "img/services/service-1.jpg",
+    },
+    {
+      id: 2,
+      title: "Remodelación de piscinas",
+      alt: "Servicio de Remodelación",
+      imgSource: "img/services/service-2.jpg",
+    },
+    {
+      id: 3,
+      title: "Seguridad de piscinas",
+      alt: "Servicio de Seguridad",
+      imgSource: "img/services/service-3.jpg",
+    },
+    {
+      id: 4,
+      title: "Protección de las piscinas",
+      alt: "Servicio de Protección",
+      imgSource: "img/services/service-4.jpg",
+    },
+  ],
+  EN: [
+    {
+      id: 1,
+      title: "Pool Construction",
+      alt: "Construction Service",
+      imgSource: "img/services/service-1.jpg",
+    },
+    {
+      id: 2,
+      title: "Pool Remodeling",
+      alt: "Remodeling Service",
+      imgSource: "img/services/service-2.jpg",
+    },
+    {
+      id: 3,
+      title: "Pool Safety",
+      alt: "Safety Service",
+      imgSource: "img/services/service-3.jpg",
+    },
+    {
+      id: 4,
+      title: "Pool Protection",
+      alt: "Protection Service",
+      imgSource: "img/services/service-4.jpg",
+    },
+  ],
+};
 
-export const SERVICES_FOOTER_DATA = [
-  {
-    id: 1,
-    icon: SVG_ICONS.WARRANTY,
-    title: "10 AÑOS DE GARANTIA O MÁS",
-  },
-  {
-    id: 2,
-    icon: SVG_ICONS.CREDIT_CARD,
-    title: "TODOS LOS MEDIOS DE PAGO",
-  },
-  {
-    id: 3,
-    icon: SVG_ICONS.TECH_ADVICE,
-    title: "ASESORAMIENTO Y MANTENIMIENTO",
-  },
-  {
-    id: 4,
-    icon: SVG_ICONS.LOCK,
-    title: "PROCESO SEGURO y 100% LEGAL",
-  },
-];
+export const SERVICES_FOOTER_DATA = {
+  ES: [
+    {
+      id: 1,
+      icon: SVG_ICONS.WARRANTY,
+      title: "10 AÑOS DE GARANTIA O MÁS",
+    },
+    {
+      id: 2,
+      icon: SVG_ICONS.CREDIT_CARD,
+      title: "TODOS LOS MEDIOS DE PAGO",
+    },
+    {
+      id: 3,
+      icon: SVG_ICONS.TECH_ADVICE,
+      title: "ASESORAMIENTO Y MANTENIMIENTO",
+    },
+    {
+      id: 4,
+      icon: SVG_ICONS.LOCK,
+      title: "PROCESO SEGURO y 100% LEGAL",
+    },
+  ],
+  EN: [
+    {
+      id: 1,
+      icon: SVG_ICONS.WARRANTY,
+      title: "10 YEARS WARRANTY OR MORE",
+    },
+    {
+      id: 2,
+      icon: SVG_ICONS.CREDIT_CARD,
+      title: "ALL PAYMENT METHODS ACCEPTED",
+    },
+    {
+      id: 3,
+      icon: SVG_ICONS.TECH_ADVICE,
+      title: "CONSULTATION AND MAINTENANCE",
+    },
+    {
+      id: 4,
+      icon: SVG_ICONS.LOCK,
+      title: "SECURE AND 100% LEGAL PROCESS",
+    },
+  ],
+};
