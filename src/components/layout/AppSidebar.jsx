e0ce7334089d3fb120b790d91b58b2f365b74f16
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-11-25 14:20:14 -0500

    Feat: Implemented translation

--- src/components/layout/AppSidebar.jsx
+++ src/components/layout/AppSidebar.jsx
@@ -3,8 +3,11 @@ import "../../styles/layout/AppSidebar.css";
 import SimpleButton from "../shared/SimpleButton";
 import { SVG_ICONS } from "../../helpers/svgIcons";
 import AppSidebarButton from "./AppSidebarButton";
+import { useTranslation } from "react-i18next";
 
 const AppSidebar = ({ isSidebarOpen, closeSidebar }) => {
+  const [t] = useTranslation("global");
+
   return (
     <aside className={isSidebarOpen ? "sidebar sidebar-active" : "sidebar"}>
       <div className="sidebar-header">
@@ -19,7 +22,7 @@ const AppSidebar = ({ isSidebarOpen, closeSidebar }) => {
         </div>
         <SimpleButton
           id="toggle-sidebar"
-          title={"Cerrar sidebar"}
+          title={t("HEADER.CLOSE-SIDEBAR-BUTTON-TITLE")}
           icon={SVG_ICONS.CLOSE}
           action={closeSidebar}
         />
@@ -29,22 +32,22 @@ const AppSidebar = ({ isSidebarOpen, closeSidebar }) => {
         <ul className="sidebar-items">
           <AppSidebarButton
             to="#section-1"
-            title="Inicio"
+            title={t("HEADER.NAV-BUTTON-HOME")}
             action={closeSidebar}
           />
           <AppSidebarButton
             to="#section-2"
-            title="Servicios"
+            title={t("HEADER.NAV-BUTTON-SERVICES")}
             action={closeSidebar}
           />
           <AppSidebarButton
             to="#section-3"
-            title="Testimonios"
+            title={t("HEADER.NAV-BUTTON-REVIEWS")}
             action={closeSidebar}
           />
           <AppSidebarButton
             to="#section-4"
-            title="Contacto"
+            title={t("HEADER.NAV-BUTTON-CONTACT")}
             action={closeSidebar}
           />
         </ul>
