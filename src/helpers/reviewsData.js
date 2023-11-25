84226a4ab814f88bb148a78a5b7242a995b201c5
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-11-25 15:08:24 -0500

    Feat: Translated review data items

--- src/helpers/reviewsData.js
+++ src/helpers/reviewsData.js
@@ -1,38 +1,78 @@
-export const REVIEWS_DATA = [
-  {
-    id: 1,
-    imgSource: "img/customers/adolf.jfif",
-    alt: "Imagen de la opinión de Adolf",
-    username: "Adolf Gomez",
-    description:
-      "Increíble trabajo de remodelación de piscinas por parte de esta empresa. Me encantó la atención al detalle y la eficiencia con la que llevaron a cabo el proyecto.",
-    rating: 5,
-  },
-  {
-    id: 2,
-    imgSource: "img/customers/clarence.jfif",
-    alt: "Imagen de la opinión de Clarence",
-    username: "Clarence Smith",
-    description:
-      "¡Excelente servicio de reparación de piscinas! Rápidos, eficientes y profesionales. Solucionaron el problema con mi piscina en poco tiempo.",
-    rating: 4,
-  },
-  {
-    id: 3,
-    imgSource: "img/customers/melissa.jfif",
-    alt: "Imagen de la opinión de Melissa",
-    username: "Melissa Kim",
-    description:
-      "Mi experiencia con esta empresa en la protección de piscinas fue fantástica. El equipo fue amable y explicó todo el proceso de manera clara. ¡Gracias por un servicio excepcional!",
-    rating: 5,
-  },
-  {
-    id: 4,
-    imgSource: "img/customers/thomas.jfif",
-    alt: "Imagen de la opinión de Thomas",
-    username: "Thomas Lotin",
-    description:
-      "Contraté y cumplieron todas mis expectativas y más. Profesionales, creativos y orientados al cliente. ¡Recomendaría sus servicios a cualquiera!",
-    rating: 5,
-  },
-];
+export const REVIEWS_DATA = {
+  ES: [
+    {
+      id: 1,
+      imgSource: "img/customers/adolf.jfif",
+      alt: "Imagen de la opinión de Adolf",
+      username: "Adolf Gomez",
+      description:
+        "Increíble trabajo de remodelación de piscinas por parte de esta empresa. Me encantó la atención al detalle y la eficiencia con la que llevaron a cabo el proyecto.",
+      rating: 5,
+    },
+    {
+      id: 2,
+      imgSource: "img/customers/clarence.jfif",
+      alt: "Imagen de la opinión de Clarence",
+      username: "Clarence Smith",
+      description:
+        "¡Excelente servicio de reparación de piscinas! Rápidos, eficientes y profesionales. Solucionaron el problema con mi piscina en poco tiempo.",
+      rating: 4,
+    },
+    {
+      id: 3,
+      imgSource: "img/customers/melissa.jfif",
+      alt: "Imagen de la opinión de Melissa",
+      username: "Melissa Kim",
+      description:
+        "Mi experiencia con esta empresa en la protección de piscinas fue fantástica. El equipo fue amable y explicó todo el proceso de manera clara. ¡Gracias por un servicio excepcional!",
+      rating: 5,
+    },
+    {
+      id: 4,
+      imgSource: "img/customers/thomas.jfif",
+      alt: "Imagen de la opinión de Thomas",
+      username: "Thomas Lotin",
+      description:
+        "Contraté y cumplieron todas mis expectativas y más. Profesionales, creativos y orientados al cliente. ¡Recomendaría sus servicios a cualquiera!",
+      rating: 5,
+    },
+  ],
+  EN: [
+    {
+      id: 1,
+      imgSource: "img/customers/adolf.jfif",
+      alt: "Image of Adolf's review",
+      username: "Adolf Gomez",
+      description:
+        "Incredible pool remodeling work by this company. I loved the attention to detail and the efficiency with which they carried out the project.",
+      rating: 5,
+    },
+    {
+      id: 2,
+      imgSource: "img/customers/clarence.jfif",
+      alt: "Image of Clarence's review",
+      username: "Clarence Smith",
+      description:
+        "Excellent pool repair service! Fast, efficient, and professional. They solved the problem with my pool in no time.",
+      rating: 4,
+    },
+    {
+      id: 3,
+      imgSource: "img/customers/melissa.jfif",
+      alt: "Image of Melissa's review",
+      username: "Melissa Kim",
+      description:
+        "My experience with this company in pool protection was fantastic. The team was friendly and explained the entire process clearly. Thanks for exceptional service!",
+      rating: 5,
+    },
+    {
+      id: 4,
+      imgSource: "img/customers/thomas.jfif",
+      alt: "Image of Thomas's review",
+      username: "Thomas Lotin",
+      description:
+        "I hired them, and they met all my expectations and more. Professional, creative, and customer-oriented. I would recommend their services to anyone!",
+      rating: 5,
+    },
+  ],
+};
